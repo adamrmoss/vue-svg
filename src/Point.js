1709a9ec4092e2635f4d7d056b731d8ed7f3242d
Adam R Moss <adamrmoss@gmail.com>
2018-06-30 23:43:13 -0400

    Create basic *.njsproj

---
+++ src/Point.js
@@ -0,0 +1,47 @@
+﻿export default class Point
+{
+    constructor(x, y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(x, y)
+    {
+        if (x instanceof Point)
+        {
+            const point = x;
+            return new Point(this.x + point.x, this.y + point.y);
+        }
+        else if (typeof x === 'number' && typeof y === 'number')
+        {
+            return new Point(this.x + x, this.y + y);
+        }
+        else
+        {
+            throw `Cannot add x: ${x} y: ${y} to ${this}`;
+        }
+    }
+
+    dot(x, y)
+    {
+        if (x instanceof Point)
+        {
+            const point = x;
+            return Math.sqrt(this.x * point.x + this.y * point.y);
+        }
+        else if (typeof x === 'number' && typeof y === 'number')
+        {
+            return Math.sqrt(this.x * x + this.y * y);
+        }
+        else
+        {
+            throw `Cannot dot product x: ${x} y: ${y} to ${this}`;
+        }
+    }
+
+    get length()
+    {
+        return this.dot(this);
+    }
+}
