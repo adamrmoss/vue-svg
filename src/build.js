fe3f4962d2b8ceb9dd5253f8cce07cda5857882a
Adam R Moss <adamrmoss@gmail.com>
2018-07-02 19:26:58 -0400

    Hmmm...

---
+++ src/build.js
@@ -0,0 +1,10 @@
+({
+    appDir: './',
+    baseUrl: './',
+    dir: '../dist/',
+    keepBuildDir: false,
+    optimize: 'none',
+    useStrict: true,
+    removeCombined: true,
+    name: 'index'
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-//exports.Point = require('./Point.js').Point;
-//exports.SvgTransform = require('./SvgTransform.js').SvgTransform;
-//exports.Animation = require('./Animation.js').Animation;
-//exports.SvgCircle = require('./SvgCircle.js').SvgCircle;
-//exports.SvgEllipse = require('./SvgEllipse.js').SvgEllipse;
-//exports.SvgLine = require('./SvgLine.js').SvgLine;
-//exports.SvgPolygon = require('./SvgPolygon.js').SvgPolygon;
-//exports.SvgPolyline = require('./SvgPolyline.js').SvgPolyline;
-//exports.SvgRect = require('./SvgRect.js').SvgRect;
+exports.Point = require('./Point.js').Point;
+exports.SvgTransform = require('./SvgTransform.js').SvgTransform;
+exports.Animation = require('./Animation.js').Animation;
+exports.SvgCircle = require('./SvgCircle.js').SvgCircle;
+exports.SvgEllipse = require('./SvgEllipse.js').SvgEllipse;
+exports.SvgLine = require('./SvgLine.js').SvgLine;
+exports.SvgPolygon = require('./SvgPolygon.js').SvgPolygon;
+exports.SvgPolyline = require('./SvgPolyline.js').SvgPolyline;
+exports.SvgRect = require('./SvgRect.js').SvgRect;
+
+
+//define(
+//    ['Point', 'SvgTransform', 'Animation', 'SvgCircle', 'SvgEllipse', 'SvgLine', 'SvgPolygon', 'SvgPolyline', 'SvgRect'],
+//    function(Point, SvgTransform, Animation, SvgCircle, SvgEllipse, SvgLine, SvgPolygon, SvgPolyline, SvgRect)
+//    {
+//        return {
+//            Point,
+//            SvgTransform,
+//            Animation,
+//            SvgCircle,
+//            SvgEllipse,
+//            SvgLine,
+//            SvgPolygon,
+//            SvgPolyline,
+//            SvgRect
+//        };
+//    }
+//);
